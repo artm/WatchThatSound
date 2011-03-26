2e8f7ce2661bb0295fd97fdd2096a00941d0ea7a
Artem Baguinski <artm@v2.nl>
2011-03-26 20:33:23 +0100

    export thread refactoring (simplification)

--- exportthread.cpp
+++ exportthread.cpp
@@ -29,6 +29,10 @@ protected:
 ExportThread::ExportThread(QObject *parent)
     : QThread(parent)
     , m_originalVideoFile(0)
+    , m_audio(0)
+    , m_container(0)
+    , m_videoStream(0)
+    , m_audioStream(0)
 {
 }
 
@@ -43,255 +47,229 @@ void ExportThread::configure(const QString& fname,
     m_audio = audio;
 }
 
-void ExportThread::run()
+void ExportThread::initExport()
 {
-    try {
-        emit exportProgress(0);
-
-        const char * filename = m_filename.constData();
-
-        AVOutputFormat * fmt = av_guess_format(NULL, filename, NULL);
-        if (!fmt) {
-            qWarning() << "Could not deduce output format "
-                          "from file extension: using AVI.";
-            fmt = av_guess_format("avi", NULL, NULL);
-        }
-
-        TRY_ASSERT_X(fmt, "Could not find suitable output format");
-
-        AVFormatContext * container = avformat_alloc_context();
-        TRY_ASSERT_X(container, "Memory error in ffmpeg");
-        container->oformat = fmt;
-        // apparently this is the normal ffmpeg way to set the filename
-        snprintf(container->filename, sizeof(container->filename), "%s", filename);
-
-        AVStream * video_st = NULL;
-        AVStream * audio_st = NULL;
-        if (fmt->video_codec != CODEC_ID_NONE) {
-            video_st = add_video_stream(container);
-        }
-        if (fmt->audio_codec != CODEC_ID_NONE) {
-            audio_st = add_audio_stream(container, fmt->audio_codec);
-        }
-
-        // set the output parameters (must be done even if no parameters)
-        TRY_ASSERT_X((av_set_parameters(container, NULL) >= 0),
-                     "Invalid output format parameters");
-
-        av_dump_format(container, 0, filename, 1);
-
-        /* now that all the parameters are set, we can open the audio and
-       video codecs and allocate the necessary encode buffers */
-        if (audio_st)
-            open_audio(container, audio_st);
-        if (video_st)
-            open_video(container, video_st);
-
-        TRY_ASSERT_X((avio_open(&container->pb, filename, URL_WRONLY) >= 0),
-                     QString("Could not open '%1'").arg(filename));
-
-        av_write_header(container);
-        m_originalVideoFile->seek(0);
-        m_audio->samplerClear();
-        qSort(m_sequence.begin(), m_sequence.end(), WtsAudio::startsBefore);
-        QList<WtsAudio::BufferAt *>::iterator sequenceCursor = m_sequence.begin();
-
-        double duration = (double)video_st->duration
-                * video_st->time_base.num
-                / video_st->time_base.den;
-        bool moreVideo = true, moreAudio = true;
-        while(moreVideo || moreAudio) {
-            double audio_pts = (double)audio_st->pts.val
-                    * audio_st->time_base.num
-                    / audio_st->time_base.den;
-            double video_pts = (double)video_st->pts.val
-                    * video_st->time_base.num
-                    / video_st->time_base.den;
-
-            moreAudio = audio_pts < duration;
-
-            double pts;
-
-            AVPacket packet;
-            if ( moreVideo
-                    && (!moreAudio || (video_pts < audio_pts))
-                    && (moreVideo = m_originalVideoFile->nextPacket(packet)) ) {
-                packet.stream_index = video_st->index;
-                av_interleaved_write_frame(container, &packet);
-                pts = video_pts;
-            } else if (moreAudio) {
-
-                av_init_packet(&packet);
-
-                qint64 ms = (qint64)(audio_pts * 1000.0);
-
-                while( sequenceCursor != m_sequence.end()
-                      && (*sequenceCursor)->at() <= ms ) {
-                    m_audio->samplerSchedule(*sequenceCursor);
-                    sequenceCursor++;
-                }
-
-                m_audio->samplerMix( ms, m_samples);
-
-                packet.size = avcodec_encode_audio(audio_st->codec,
-                                                   m_audioOutbuf.data(),
-                                                   m_audioOutbuf.size(),
-                                                   m_samples.data());
-                if (audio_st->codec->coded_frame
-                        && ( (uint64_t) audio_st->codec->coded_frame->pts
-                            != AV_NOPTS_VALUE) )
-                    packet.pts= av_rescale_q(audio_st->codec->coded_frame->pts,
-                                             audio_st->codec->time_base,
-                                             audio_st->time_base);
-                packet.flags |= AV_PKT_FLAG_KEY;
-                packet.stream_index= audio_st->index;
-                packet.data= m_audioOutbuf.data();
-                av_interleaved_write_frame(container, &packet);
-
-                pts = audio_pts;
-            }
-
-            emit exportProgress(100 * pts / duration);
-        }
-        av_write_trailer(container);
-
-        /* close each codec */
-        if (video_st)
-            close_video(container, video_st);
-        if (audio_st)
-            close_audio(container, audio_st);
-
-        /* free the resources */
-        for(unsigned i = 0; i < container->nb_streams; i++) {
-            av_freep(&container->streams[i]->codec);
-            av_freep(&container->streams[i]);
-        }
-
-        avio_close(container->pb);
-        av_free(container);
-
-    } catch (const AssertFailed& e) {
-        qCritical() << e.cMessage();
+    AVOutputFormat * format = av_guess_format(NULL, m_filename.constData(), NULL);
+    if (!format) {
+        qWarning() << "Could not deduce output format "
+                      "from file extension: using MOV (QuickTime).";
+        format = av_guess_format("mov", NULL, NULL);
     }
+
+    TRY_ASSERT_X(format, "Could not find suitable output format");
+
+    m_container = avformat_alloc_context();
+    TRY_ASSERT_X(m_container, "Memory error in ffmpeg");
+    m_container->oformat = format;
+    // apparently this is the normal ffmpeg way to set the filename
+    snprintf(m_container->filename,
+             sizeof(m_container->filename),
+             "%s",
+             m_filename.constData());
+
+    if (format->audio_codec != CODEC_ID_NONE)
+        initAudioStream(format->audio_codec);
+    if (format->video_codec != CODEC_ID_NONE)
+        initVideoStream();
+
+
+    // set the output parameters (must be done even if no parameters)
+    TRY_ASSERT_X((av_set_parameters(m_container, NULL) >= 0),
+                 "Invalid output format parameters");
+
+    av_dump_format(m_container, 0, m_filename.constData(), 1);
+
+    AVCodec * audioCodec = avcodec_find_encoder(m_audioStream->codec->codec_id);
+    TRY_ASSERT_X(audioCodec, "Could not find audio encoder");
+    TRY_ASSERT_X((avcodec_open(m_audioStream->codec, audioCodec) >= 0),
+                 "Could not open audio encoder");
+
+    AVCodec * videoCodec = avcodec_find_encoder(m_videoStream->codec->codec_id);
+    TRY_ASSERT_X(videoCodec, "Video codec not found");
+    TRY_ASSERT_X((avcodec_open(m_videoStream->codec, videoCodec) >= 0),
+                 "Could not open video codec");
+
+    TRY_ASSERT_X( ( avio_open(&m_container->pb,
+                              m_filename.constData(),
+                              URL_WRONLY) >= 0 ),
+                 QString("Could not open " + m_filename));
+
 }
 
-AVStream * ExportThread::add_audio_stream(AVFormatContext *oc, enum CodecID codec_id)
+void ExportThread::performExport()
 {
-    AVCodecContext *c;
-    AVStream *st;
+    QVector<uint8_t> m_encodedAudio(10000);
 
-    st = av_new_stream(oc, 1);
-    TRY_ASSERT_X(st, "Could not alloc stream");
-
-    c = st->codec;
-    c->codec_id = codec_id;
-    c->codec_type = AVMEDIA_TYPE_AUDIO;
-
-    /* put sample parameters */
-    c->sample_fmt = AV_SAMPLE_FMT_S16;
-    c->bit_rate = 64000;
-    c->sample_rate = 44100;
-    c->channels = 1;
-
-    // some formats want stream headers to be separate
-    if(oc->oformat->flags & AVFMT_GLOBALHEADER)
-        c->flags |= CODEC_FLAG_GLOBAL_HEADER;
-
-    return st;
-}
-
-void ExportThread::open_audio(AVFormatContext */*oc*/, AVStream *st)
-{
-    AVCodecContext *c;
-    AVCodec *codec;
-
-    c = st->codec;
-
-    codec = avcodec_find_encoder(c->codec_id);
-    TRY_ASSERT_X(codec, "Could not find audio encoder");
-    TRY_ASSERT_X((avcodec_open(c, codec) >= 0), "Could not open audio encoder");
-
-    m_audioOutbuf.resize(10000);
-
-    /* ugly hack for PCM codecs (will be removed ASAP with new PCM
-       support to compute the input frame size in samples */
-    int inputFrameSize = c->frame_size;
-    if (c->frame_size <= 1) {
-        inputFrameSize = m_audioOutbuf.size() / c->channels;
-        switch(st->codec->codec_id) {
+    // this is copied from ffmpeg's output-example
+    // original comment states that it's an "ugly hack for PCM codecs"
+    int mixBufferSize = m_audioStream->codec->frame_size;
+    if (m_audioStream->codec->frame_size <= 1) {
+        mixBufferSize = m_encodedAudio.size() / m_audioStream->codec->channels;
+        switch(m_audioStream->codec->codec_id) {
         case CODEC_ID_PCM_S16LE:
         case CODEC_ID_PCM_S16BE:
         case CODEC_ID_PCM_U16LE:
         case CODEC_ID_PCM_U16BE:
-            inputFrameSize >>= 1;
+            mixBufferSize >>= 1;
             break;
         default:
             break;
         }
     }
 
-    m_samples.resize(inputFrameSize * c->channels);
+    QVector<int16_t> m_mixBuffer(mixBufferSize * m_audioStream->codec->channels);
+
+    av_write_header(m_container);
+
+    m_originalVideoFile->seek(0);
+    m_audio->samplerClear();
+    qSort(m_sequence.begin(), m_sequence.end(), WtsAudio::startsBefore);
+    QList<WtsAudio::BufferAt *>::iterator sequenceCursor = m_sequence.begin();
+
+    double duration = (double)m_videoStream->duration
+            * m_videoStream->time_base.num
+            / m_videoStream->time_base.den;
+    bool moreVideo = true, moreAudio = true;
+    while(moreVideo || moreAudio) {
+        double audio_pts = (double)m_audioStream->pts.val
+                * m_audioStream->time_base.num
+                / m_audioStream->time_base.den;
+        double video_pts = (double)m_videoStream->pts.val
+                * m_videoStream->time_base.num
+                / m_videoStream->time_base.den;
+
+        moreAudio = audio_pts < duration;
+
+        double pts;
+
+        AVPacket packet;
+        if ( moreVideo
+                && (!moreAudio || (video_pts < audio_pts))
+                && (moreVideo = m_originalVideoFile->nextPacket(packet)) ) {
+            packet.stream_index = m_videoStream->index;
+            av_interleaved_write_frame(m_container, &packet);
+            pts = video_pts;
+        } else if (moreAudio) {
+
+            av_init_packet(&packet);
+
+            qint64 ms = (qint64)(audio_pts * 1000.0);
+
+            while( sequenceCursor != m_sequence.end()
+                  && (*sequenceCursor)->at() <= ms ) {
+                m_audio->samplerSchedule(*sequenceCursor);
+                sequenceCursor++;
+            }
+
+            m_audio->samplerMix( ms, m_mixBuffer);
+
+            packet.size = avcodec_encode_audio(m_audioStream->codec,
+                                               m_encodedAudio.data(),
+                                               m_encodedAudio.size(),
+                                               m_mixBuffer.data());
+            if (m_audioStream->codec->coded_frame
+                    && ( (uint64_t) m_audioStream->codec->coded_frame->pts
+                        != AV_NOPTS_VALUE) )
+                packet.pts= av_rescale_q(m_audioStream->codec->coded_frame->pts,
+                                         m_audioStream->codec->time_base,
+                                         m_audioStream->time_base);
+            packet.flags |= AV_PKT_FLAG_KEY;
+            packet.stream_index= m_audioStream->index;
+            packet.data= m_encodedAudio.data();
+            av_interleaved_write_frame(m_container, &packet);
+
+            pts = audio_pts;
+        }
+
+        emit exportProgress(100 * pts / duration);
+    }
+
+    av_write_trailer(m_container);
 }
 
-void ExportThread::close_audio(AVFormatContext */*oc*/, AVStream *st)
+void ExportThread::run()
 {
-    avcodec_close(st->codec);
-    m_samples.resize(0);
-    m_audioOutbuf.resize(0);
+    try {
+        emit exportProgress(0);
+
+        initExport();
+        performExport();
+        finishUp();
+
+    } catch (const AssertFailed& e) {
+        qCritical() << e.cMessage();
+    }
 }
 
-AVStream * ExportThread::add_video_stream(AVFormatContext * container)
+void ExportThread::finishUp()
+{
+    avcodec_close(m_videoStream->codec);
+    avcodec_close(m_audioStream->codec);
+
+    /* free the resources */
+    for(unsigned i = 0; i < m_container->nb_streams; i++) {
+        av_freep(&m_container->streams[i]->codec);
+        av_freep(&m_container->streams[i]);
+    }
+
+    avio_close(m_container->pb);
+    av_free(m_container);
+}
+
+void ExportThread::initAudioStream(CodecID codec_id)
+{
+    AVCodecContext * codecContext;
+
+    m_audioStream = av_new_stream(m_container, 1);
+    TRY_ASSERT_X(m_audioStream, "Could not alloc stream");
+
+    codecContext = m_audioStream->codec;
+    codecContext->codec_id = codec_id;
+    codecContext->codec_type = AVMEDIA_TYPE_AUDIO;
+
+    /* put sample parameters */
+    codecContext->sample_fmt = AV_SAMPLE_FMT_S16;
+    codecContext->bit_rate = 64000;
+    codecContext->sample_rate = 44100;
+    codecContext->channels = 1;
+
+    // some formats want stream headers to be separate
+    if(m_container->oformat->flags & AVFMT_GLOBALHEADER)
+        codecContext->flags |= CODEC_FLAG_GLOBAL_HEADER;
+}
+
+void ExportThread::initVideoStream()
 {
     // Video output stream is pretty much a copy of the input video
-    AVStream * newStream = av_new_stream(container, 0);
-    TRY_ASSERT_X(newStream, "Could not allocate memory");
-    // this technique is copied from glorious FFmpeg
-    //memcpy(newStream, m_videoFile->stream(), sizeof(AVStream));
+    m_videoStream = av_new_stream(m_container, 0);
+    TRY_ASSERT_X(m_videoStream, "Could not allocate memory");
 
     const AVStream * oldStream = m_originalVideoFile->stream();
     // we copy some fields but leave other as they were initialized by
     // av_new_stream() above. this way we're sure that no pointers to
     // malloced memory get copied and later freed twice.
-    newStream->r_frame_rate = oldStream->r_frame_rate;
-    newStream->first_dts = oldStream->first_dts;
-    newStream->pts = oldStream->pts;
-    newStream->time_base = oldStream->time_base;
-    newStream->pts_wrap_bits = oldStream->pts_wrap_bits;
-    newStream->discard = oldStream->discard;
-    newStream->quality = oldStream->quality;
-    newStream->start_time = oldStream->start_time;
-    newStream->duration = oldStream->duration;
-    newStream->nb_frames = oldStream->nb_frames;
-    newStream->disposition = oldStream->disposition;
-    newStream->sample_aspect_ratio = oldStream->sample_aspect_ratio;
-    newStream->avg_frame_rate = oldStream->avg_frame_rate;
+    m_videoStream->r_frame_rate = oldStream->r_frame_rate;
+    m_videoStream->first_dts = oldStream->first_dts;
+    m_videoStream->pts = oldStream->pts;
+    m_videoStream->time_base = oldStream->time_base;
+    m_videoStream->pts_wrap_bits = oldStream->pts_wrap_bits;
+    m_videoStream->discard = oldStream->discard;
+    m_videoStream->quality = oldStream->quality;
+    m_videoStream->start_time = oldStream->start_time;
+    m_videoStream->duration = oldStream->duration;
+    m_videoStream->nb_frames = oldStream->nb_frames;
+    m_videoStream->disposition = oldStream->disposition;
+    m_videoStream->sample_aspect_ratio = oldStream->sample_aspect_ratio;
+    m_videoStream->avg_frame_rate = oldStream->avg_frame_rate;
     // we're not sure this makes any difference but just to be nice
-    newStream->stream_copy = 1;
+    m_videoStream->stream_copy = 1;
 
     // copy codec from input file
-    newStream->codec = avcodec_alloc_context();
-    TRY_ASSERT_X(newStream->codec, "Could not allocate memory");
-    avcodec_copy_context(newStream->codec, m_originalVideoFile->codec());
+    m_videoStream->codec = avcodec_alloc_context();
+    TRY_ASSERT_X(m_videoStream->codec, "Could not allocate memory");
+    avcodec_copy_context(m_videoStream->codec, m_originalVideoFile->codec());
     // can't copy mjpeg without this
-    newStream->codec->strict_std_compliance = FF_COMPLIANCE_UNOFFICIAL;
-    newStream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
+    m_videoStream->codec->strict_std_compliance = FF_COMPLIANCE_UNOFFICIAL;
+    m_videoStream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
 
-    return newStream;
-}
-
-void ExportThread::open_video(AVFormatContext * /*container*/, AVStream *stream)
-{
-    AVCodecContext * codecContext = stream->codec;
-
-    /* find the video encoder */
-    AVCodec * codec = avcodec_find_encoder(codecContext->codec_id);
-    TRY_ASSERT_X(codec, "Video codec not found");
-    TRY_ASSERT_X((avcodec_open(codecContext, codec) >= 0),
-                 "Could not open video codec");
-
-}
-
-void ExportThread::close_video(AVFormatContext */*oc*/, AVStream *st)
-{
-    avcodec_close(st->codec);
 }
--- exportthread.h
+++ exportthread.h
@@ -31,20 +31,21 @@ signals:
 public slots:
 
 protected:
-    AVStream *add_audio_stream(AVFormatContext *oc, enum CodecID codec_id);
-    void open_audio(AVFormatContext *oc, AVStream *st);
-    void close_audio(AVFormatContext *oc, AVStream *st);
-    AVStream *add_video_stream(AVFormatContext *oc);
-    void open_video(AVFormatContext *oc, AVStream *st);
-    void close_video(AVFormatContext *oc, AVStream *st);
+    void initExport();
+    void initAudioStream(CodecID codec_id);
+    void initVideoStream();
+    void performExport();
+    void finishUp();
 
     QByteArray m_filename;
     VideoFile * m_originalVideoFile;
     QList<WtsAudio::BufferAt *> m_sequence;
     WtsAudio * m_audio;
 
-    QVector<int16_t> m_samples;
-    QVector<uint8_t> m_audioOutbuf;
+    AVFormatContext * m_container;
+    AVStream * m_videoStream;
+    AVStream * m_audioStream;
+
 };
 
 #endif // EXPORTTHREAD_H
