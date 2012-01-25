b84cc46a4f5053462559924f50d92db0fbf0f77c
Artem Baguinski <artm@v2.nl>
2012-01-25 11:44:25 +0100

    No spaces in app filename / clean up
    this was breaking 'make clean' and I have no desire to fix it.

--- SequencerTimeLine.h
+++ SequencerTimeLine.h
@@ -40,10 +40,6 @@ protected:
     void restackItems();
     void showRange(QGraphicsItem * root, SoundBuffer * buffer);
 
-    /*
-    virtual void mousePressEvent ( QMouseEvent * event );
-    virtual void mouseMoveEvent ( QMouseEvent * event );
-    */
     virtual void mouseReleaseEvent ( QMouseEvent * event );
 };
 
